804d2246b61f0a2a6567b82448abc03c3673afda
Loginworks Softwares <38474202+amitpmloginworks@users.noreply.github.com>
2019-02-22 19:54:01 +0530

    reedem changes

--- api/controllers/listplaces.js
+++ api/controllers/listplaces.js
@@ -1,14 +1,19 @@
 const Places=require('../../models/qrcodegenerator');
 
 const qrcodelist=require('../../controllers/show_dashboard')
-
+const Reedemcard=require('../models/reedemcard');
 exports.listqrcode=(req,res,next)=>{
-  
+ Reedemcard.find().then(data=>{
     Places.find().then(qrcodeparam=>{
         console.log('name',qrcodeparam)
    return res.status(200).json({
-        qrcodename:qrcodeparam
+        qrcodename:qrcodeparam,
+        data:data
     })    
-    })
+    })  
+ })
+  
+
+ 
 }
 
